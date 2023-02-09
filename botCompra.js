c1dd4fd3b941b8c36f6c41656e8d72e62b07d2f2
Xrd3000 <xrdd3000@gmail.com>
2023-02-08 22:42:28 -0300

    se le agrego la funcion de proxys

--- botCompra.js
+++ botCompra.js
@@ -1,8 +1,21 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until, Options} = require('selenium-webdriver');
 require("chromedriver")
 
+
+/*String_proxyAPICall = "http://pubproxy.com/api/proxy?format=txt"
+if (driver.findElementsByTagName("pre").size() > 0) {
+pubProxy = driver.findElementByXPath("/html/body/pre").getText();
+// set the proxy
+options = new ChromeOptions().addArguments("--proxy-server=" + pubProxy);
+System.out.println("proxy: ".concat(pubProxy));
+}*/
+
 async function bot(){
-    let driver = await new Builder().forBrowser('chrome').build();
+    let proxyAddress = '212.56.139.253:80';
+    let option = new Chrome.Options().addArguments(`--proxy-server=http://${proxyAddress}`);
+
+    let driver = await new Builder().forBrowser('chrome').build()
+    .setChromeOptions(option)
     await driver.get('https://www.nike.cl/login');
     await driver.findElement(By.id('vtexIdContainer'));
     const entrar = await driver.findElement(By.id("vtexIdUI-custom-oauth"));
@@ -17,7 +30,7 @@ async function bot(){
 
     console.log(await driver.getTitle())
     const correo = await driver.findElement(By.className('nds-input-text-field css-1yj75qi e1fiih290'))
-    await correo.sendKeys("cr.hachim2.0@gmail.com")
+    await correo.send_Keys("cr.hachim2.0@gmail.com")
     const boton_correo_aceptar = await driver.findElement(By.className('css-18y7846 btn-primary-dark  btn-md'));
     boton_correo_aceptar.click()
     await new Promise(resolve => setTimeout(resolve, 10000));
